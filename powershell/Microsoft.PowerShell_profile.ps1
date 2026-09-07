81a4f440699c7b9910c9ce344e9bfd7b5813191c
Adnan Memic <adnan.memic27@gmail.com>
2026-09-08 01:54:34 +0200

    pwsh-profile: remove command-exists and module-exists checks

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -2,31 +2,23 @@
 ### Oh my posh ###
 ##################
 
-if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
-	$theme = "robbyrussell"
-	oh-my-posh init pwsh --config "$HOME\.omp-themes\$theme.omp.json" | Invoke-Expression
-}
+$theme = "robbyrussell"
+oh-my-posh init pwsh --config "$HOME\.omp-themes\$theme.omp.json" | Invoke-Expression
 
 
 #################
 ### Fastfetch ###
 #################
 
-if (Get-Command fastfetch -ErrorAction SilentlyContinue) {
-	fastfetch
-}
+fastfetch
 
 
 ###############
 ### Modules ###
 ###############
 
-if (Get-Module -ListAvailable -Name Terminal-Icons) {
-	Import-Module -Name Terminal-Icons
-}
-if (Get-Module -ListAvailable -Name Microsoft.WinGet.CommandNotFound) {
-	Import-Module -Name Microsoft.WinGet.CommandNotFound
-}
+Import-Module -Name Terminal-Icons
+Import-Module -Name Microsoft.WinGet.CommandNotFound
 
 
 ###############
